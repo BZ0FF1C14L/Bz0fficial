785b68206dbfcd141e1d43078b43a93a2465cde1
BZ0FF1C14L <138209891+BZ0FF1C14L@users.noreply.github.com>
2023-07-01 02:52:14 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -559,24 +559,24 @@ function udp_mini(){
 clear
 print_install "Memasang UDP MINI"
 # // Installing UDP Mini
-#mkdir -p /usr/local/kyt/
-#wget -q -O /usr/local/kyt/udp-mini "https://raw.githubusercontent.com/BZ0FF1C14L/Bz0fficial/main/badvpn/udp-mini"
-#chmod +x /usr/local/kyt/udp-mini
-#wget -q -O /etc/systemd/system/udp-mini-1.service "https://raw.githubusercontent.com/BZ0FF1C14L/Bz0fficial/main/badvpn/udp-mini-1.service"
-#wget -q -O /etc/systemd/system/udp-mini-2.service "https://raw.githubusercontent.com/BZ0FF1C14L/Bz0fficial/main/badvpn/udp-mini-2.service"
-#wget -q -O /etc/systemd/system/udp-mini-3.service "https://raw.githubusercontent.com/BZ0FF1C14L/Bz0fficial/main/badvpn/udp-mini-3.service"
-#systemctl disable udp-mini-1
-#systemctl stop udp-mini-1
-#systemctl enable udp-mini-1
-#systemctl start udp-mini-1
-#systemctl disable udp-mini-2
-#systemctl stop udp-mini-2
-#systemctl enable udp-mini-2
-#systemctl start udp-mini-2
-#systemctl disable udp-mini-3
-#systemctl stop udp-mini-3
-#systemctl enable udp-mini-3
-#systemctl start udp-mini-3
+mkdir -p /usr/local/kyt/
+wget -q -O /usr/local/kyt/udp-mini "https://raw.githubusercontent.com/BZ0FF1C14L/Bz0fficial/main/badvpn/udp-mini"
+chmod +x /usr/local/kyt/udp-mini
+wget -q -O /etc/systemd/system/udp-mini-1.service "https://raw.githubusercontent.com/BZ0FF1C14L/Bz0fficial/main/badvpn/udp-mini-1.service"
+wget -q -O /etc/systemd/system/udp-mini-2.service "https://raw.githubusercontent.com/BZ0FF1C14L/Bz0fficial/main/badvpn/udp-mini-2.service"
+wget -q -O /etc/systemd/system/udp-mini-3.service "https://raw.githubusercontent.com/BZ0FF1C14L/Bz0fficial/main/badvpn/udp-mini-3.service"
+systemctl disable udp-mini-1
+systemctl stop udp-mini-1
+systemctl enable udp-mini-1
+systemctl start udp-mini-1
+systemctl disable udp-mini-2
+systemctl stop udp-mini-2
+systemctl enable udp-mini-2
+systemctl start udp-mini-2
+systemctl disable udp-mini-3
+systemctl stop udp-mini-3
+systemctl enable udp-mini-3
+systemctl start udp-mini-3
 print_success "UDP MINI"
 }
 
